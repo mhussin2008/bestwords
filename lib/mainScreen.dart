958d16a37200be99558402c849507c1f6d624e6d
MohamedHussin <mhussin2008@yahoo.com>
2023-11-11 10:06:52 +0200

    first commit 2

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'data.dart';
 
 // https://www.youtube.com/@faykooo1/playlists
 
 class mainScreen extends StatefulWidget {
   final update;
+  // final
 
   mainScreen(this.update): super(key: data.gk);
 
@@ -19,7 +21,19 @@ class _mainScreenState extends State<mainScreen> {
     return   Center(
       child: Column(
         children: [
-          ElevatedButton(onPressed: (){widget.update();}, child: Text('update parent')),
+          ElevatedButton(onPressed: () async {
+            AssetsAudioPlayer.newPlayer().open(
+              Audio('assets/mp3_original/1-split/1_silence_01.mp3'),
+              autoStart: true,
+              showNotification: true,
+            );
+
+
+
+            //widget.player.audioSource=AudioSource.asset('assets/mp3_original/1-split/1_silence_01.mp3');
+             }
+
+             ,child: Text('تشغيل'),),
 
           Image(image: AssetImage('assets/images/${data.pageNumber}.png')
           ,width:MediaQuery.of(context).size.width ,
